4b5bcbd286f9579a27cec714916d99f89b152055
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-08-25 11:33:42 +0200

    Adds Laravel Sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Enable stateful API auth for Sanctum
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
