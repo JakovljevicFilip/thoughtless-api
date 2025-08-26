d43f954af2e433f29251793390c0e0e9ff1b4fde
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-08-26 14:32:24 +0200

    Changes User id from int to uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-
     public const MAX_FIRST_NAME_LENGTH = 100;
     public const MAX_LAST_NAME_LENGTH  = 100;
     public const MAX_EMAIL_LENGTH      = 255;
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * UUID is not auto-incrementing, so we disable incrementing
+     * and set the key type to string.
+     */
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
@@ -49,5 +56,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Auto-generate UUID when creating new users.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
     }
 }
